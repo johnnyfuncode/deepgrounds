23e717b156a8f39c00cdacff3c0db9e32c25fad1
johnnyfuncode <johngofongo@yahoo.com>
2013-09-21 15:40:41 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deepgrounds::Application.config.secret_key_base = '6b93e448a9ac242a48729a08ebc29ea447d68ccd4e7eacdf978c5813a018d9512db3307fcaebc48aaf58af6b4137428071cbf483ded325a8a6f6055219a98d15'
